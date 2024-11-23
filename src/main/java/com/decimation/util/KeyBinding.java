a55d196e707247f5bc366f5aee2c09295767a943
Uncody <timatop2020jjj@gmail.com>
2024-11-24 00:02:05 +0300

    WIP, testing keybinds

--- src/main/java/com/decimation/util/KeyBinding.java
+++ src/main/java/com/decimation/util/KeyBinding.java
@@ -1,6 +1,16 @@
 package com.decimation.util;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
 public class KeyBinding {
     public static final String KEY_CATEGORY_DECIMATION = "key.category.decimation.decimation";
     public static final String KEY_TEST = "key.decimation.test_button";
+
+
+    public static final KeyMapping TEST_KEY = new KeyMapping(KEY_TEST, KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, KEY_CATEGORY_DECIMATION);
+
 }
